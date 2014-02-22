693adb6dd9f4c3988e50fe41f326a2fff5b87e48
Joey Figaro <joey@joeyfigaro.com>
2014-02-22 16:01:06 -0500

    completed!

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="stylesheet" href="assets/stylesheets/app.css">
-        <script src="js/vendor/modernizr-2.7.1.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 8]>
@@ -16,7 +15,10 @@
         <![endif]-->
 
         <header>
-            <h1>Assembly Watch</h1>
+            <img class="logo" src="assets/images/logo.png">
+            <div class="search">
+                <em>examples: 'HB100', 'Opportunity funding', '2.2-115'</em>
+            </div>
         </header>
         <main>
             <?php
@@ -41,19 +43,18 @@
 
                 $data = clean_json($data, 'bill_catch_line');
             ?>
-            <pre><?php // var_dump($data[0]); ?></pre>
             <table>
                 <thead>
                     <tr>
-                        <th>Number</th>
-                        <th>Description</th>
-                        <th>Law</th>
+                        <th class="number">Number</th>
+                        <th class="description">Description</th>
+                        <th class="law">Law</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php foreach($data as $item) { ?>
                     <tr class="bill">
-                        <td class="number"><?php echo $item['bill_number']; ?></td>
+                        <td class="number"><span><?php echo $item['bill_number']; ?></span></td>
                         <td class="description"><a href="<?php echo $item['url']; ?>" target="_blank"><?php echo $item['bill_catch_line']; ?></a></td>
                         <td class="law"><?php echo $item['law']; ?></td>
                     </tr>
@@ -65,6 +66,19 @@
 
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="assets/js/data-tables.js"></script>
+        <script>
+            $(function() {
+                $('table').dataTable({
+                    // flp, rt, i
+                    "sDom": 'tip <"hidden"f>'
+                });
+
+                var search = $('.dataTables_filter input');
+                search.attr('placeholder', 'find a bill');
+                $('header .search').prepend(search);
+            });
+        </script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
